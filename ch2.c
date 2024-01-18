133a4378dc76d8cebeafd2dd879b827cd7ec6487
Fabio Hernan Gomez Rubio <fabiogomez11c@gmail.com>
2024-01-18 08:49:56 -0500

    exc 2

---
+++ ch2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    char ch;
+    scanf("%c", &ch);
+    printf("%c\n", ch);
+
+    char s[100];
+    scanf("%99s", s);
+    printf("%s\n", s);
+    scanf("\n");
+
+    char sen[100];
+    scanf("%[^\n]%*c", sen);
+    printf("%s", sen);
+
+    return 0;
+}
