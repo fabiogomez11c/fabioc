9d1b5fca30c6ee00fd4d4836cb382023be91efde
Fabio Hernan Gomez Rubio <fabiogomez11c@gmail.com>
2024-01-22 11:12:43 -0500

    ch7 done

--- ch7.c
+++ ch7.c
@@ -6,22 +6,19 @@
 int main() {
 
     int n;
-    // scanf("%d", &n);
-    n = 10564;
+    scanf("%d", &n);
     //Complete the code to calculate the sum of the five digits on n.
     int cum = 0;
     int mod;
     int div;
-    int divisor = 10000;
+    int divisor = 10;
 
     mod = n;
-    for (int i=1; i < 7; i++){
-        // get the first number
-        div = mod / divisor;
+    for (int i=1; i < 6; i++){
+        // get the last number
+        div = mod % divisor;
         // get the modulus
-        mod = mod % divisor;
-        // udpate divisor
-        divisor = divisor/10;
+        mod = mod / divisor;
         // update cum sum
         cum += div;
     }
