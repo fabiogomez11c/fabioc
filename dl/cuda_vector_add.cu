4410d5aa74961213e7487fe8f9f734d0c820868f
Fabio Gomez <fabiogomez11c@gmail.com>
2024-01-23 01:47:05 +0000

    cuda malloc cuda free

---
+++ dl/cuda_vector_add.cu
@@ -0,0 +1,33 @@
+// this is a modification of the vector_add.c with cuda kernel implemented
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+void vecAdd(float* A_h, float* B_h, float* C_h, int n){
+    int size = n * sizeof(float); // size of a float data type * the length of the array
+    float *A_d, *B_d, *C_d;
+
+    // part 1: allocate device memory for A, B and C
+    // copy A and B in device memory
+    cudaMalloc((void**)&A_d, size);
+    cudaMalloc((void**)&B_d, size);
+    cudaMalloc((void**)&C_d, size);
+
+    cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
+
+    // part 2: call kernel - to launch a grid of kernels
+    // to perform the actual vector addition
+    
+    // part 3: copy c from the device memory
+    // free device vectors
+    cudaMemcpy(C_d, C_h, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(A_d);
+    cudaFree(B_d);
+    cudaFree(C_d);
+}
+
+int main()
+{
+    return 0;
+}
